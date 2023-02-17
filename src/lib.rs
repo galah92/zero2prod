4c939b7ca1a30df71ee23b19cdcdcbf090a08229
galah92 <galaharoni92@gmail.com>
2023-02-17 14:06:04 -0500

    switched from actix-web builder pattern to marcos

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
 
+#[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
 pub fn app_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/health_check").route(web::get().to(health_check)));
+    cfg.service(health_check);
 }
