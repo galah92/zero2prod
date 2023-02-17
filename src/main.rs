d425d0812f97f2a7daded6a2c4a99ce38685ffa9
galah92 <galaharoni92@gmail.com>
2023-02-17 13:43:51 -0500

    added test for /health_check

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,18 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http;
+    use actix_web::test;
+
+    #[actix_web::test]
+    async fn health_check_works() {
+        let app = test::init_service(App::new().configure(config)).await;
+        let req = test::TestRequest::get().uri("/health_check").to_request();
+        let res = test::call_service(&app, req).await;
+        assert_eq!(res.status(), http::StatusCode::OK);
+    }
+}
