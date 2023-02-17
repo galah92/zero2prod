cd5c7b2e5f44a20e85228c66b3dc9760f7be6097
galah92 <galaharoni92@gmail.com>
2023-02-17 14:56:44 -0500

    added stub implementation for /subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
+use serde::Deserialize;
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+#[post("/subscriptions")]
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+    format!("Welcome, {} <{}>!", form.name, form.email)
+}
+
 pub fn app_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(health_check);
+    cfg.service(health_check).service(subscribe);
 }
