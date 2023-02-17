9c141831b398880e3e3c2d34b3da06d368b369b4
galah92 <galaharoni92@gmail.com>
2023-02-17 14:04:42 -0500

    removed leftover code duplication from file splitting

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, HttpResponse, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -6,12 +6,4 @@ async fn health_check() -> impl Responder {
 
 pub fn app_config(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/health_check").route(web::get().to(health_check)));
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(app_config))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
 }
