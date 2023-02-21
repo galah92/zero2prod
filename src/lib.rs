7b5259fb6fe521f52e3e562f56b2ca232732b4e6
galah92 <galaharoni92@gmail.com>
2023-02-21 09:36:52 -0500

    swapped chrono with time due to audit failure of the chrono crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,50 @@
-pub mod settings;
+mod settings;
 
 pub use settings::get_settings;
 
-use actix_web::{get, post, web, HttpResponse, Responder};
-use chrono::Utc;
-use serde::Deserialize;
-use sqlx::PgPool;
-use uuid::Uuid;
-
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+use actix_web::web;
+pub fn app_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(routes::health_check).service(routes::subscribe);
 }
 
-#[derive(Deserialize)]
-struct FormData {
-    name: String,
-    email: String,
-}
+mod routes {
+    use actix_web::{get, post, web, HttpResponse, Responder};
+    use serde::Deserialize;
+    use sqlx::PgPool;
+    use time::OffsetDateTime;
+    use uuid::Uuid;
 
-#[post("/subscriptions")]
-async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
-    let query_result = sqlx::query!(
-        r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now()
-    )
-    .execute(db_pool.get_ref())
-    .await;
-    match query_result {
-        Ok(_) => HttpResponse::Ok(),
-        Err(e) => {
-            println!("Error: {}", e);
-            HttpResponse::InternalServerError()
+    #[get("/health_check")]
+    async fn health_check() -> impl Responder {
+        HttpResponse::Ok()
+    }
+
+    #[derive(Deserialize)]
+    struct FormData {
+        name: String,
+        email: String,
+    }
+
+    #[post("/subscriptions")]
+    async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
+        let query_result = sqlx::query!(
+            r#"
+            INSERT INTO subscriptions (id, email, name, subscribed_at)
+            VALUES ($1, $2, $3, $4)
+            "#,
+            Uuid::new_v4(),
+            form.email,
+            form.name,
+            OffsetDateTime::now_utc(),
+        )
+        .execute(db_pool.get_ref())
+        .await;
+        match query_result {
+            Ok(_) => HttpResponse::Ok(),
+            Err(e) => {
+                println!("Error: {}", e);
+                HttpResponse::InternalServerError()
+            }
         }
     }
-}
-
-pub fn app_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(health_check).service(subscribe);
 }
