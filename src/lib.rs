2b0593c376a91a7a9d5cdb422bee0f4c0a44df23
galah92 <galaharoni92@gmail.com>
2023-02-20 22:42:52 -0500

    implemented db write as part of the /subscriptions API

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ pub mod settings;
 pub use settings::get_settings;
 
 use actix_web::{get, post, web, HttpResponse, Responder};
+use chrono::Utc;
 use serde::Deserialize;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
@@ -17,8 +20,26 @@ struct FormData {
 }
 
 #[post("/subscriptions")]
-async fn subscribe(form: web::Form<FormData>) -> impl Responder {
-    format!("Welcome, {} <{}>!", form.name, form.email)
+async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
+    let query_result = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(db_pool.get_ref())
+    .await;
+    match query_result {
+        Ok(_) => HttpResponse::Ok(),
+        Err(e) => {
+            println!("Error: {}", e);
+            HttpResponse::InternalServerError()
+        }
+    }
 }
 
 pub fn app_config(cfg: &mut web::ServiceConfig) {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use zero2prod::app_config;
 
 #[actix_web::main]
@@ -6,7 +7,12 @@ async fn main() -> std::io::Result<()> {
     let settings = zero2prod::get_settings().expect("Failed to read config");
     let address = format!("127.0.0.1:{}", settings.port);
 
-    HttpServer::new(|| App::new().configure(app_config))
+    let db_pool = PgPool::connect(&settings.database.connection_string())
+        .await
+        .unwrap();
+    let db_pool = web::Data::new(db_pool);
+
+    HttpServer::new(move || App::new().configure(app_config).app_data(db_pool.clone()))
         .bind(address)?
         .run()
         .await
