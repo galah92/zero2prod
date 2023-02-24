3c4b16c8f2a4996e7efc996fd0b6ce950405d242
galah92 <galaharoni92@gmail.com>
2023-02-24 16:53:11 +0000

    halfway implementation of unit test for EmailClient

--- src/email.rs
+++ src/email.rs
@@ -16,7 +16,50 @@ impl EmailClient {
         }
     }
 
-    pub async fn send_email(&self) -> Result<(), String> {
-        todo!()
+    pub async fn send_email(
+        &self,
+        _recipient: &SubscriberEmail,
+        _subject: &str,
+        _html_content: &str,
+        _text_content: &str,
+    ) -> Result<(), String> {
+        self._http_client
+            .post(&self._base_url)
+            .send()
+            .await
+            .map_err(|err| err.to_string())?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{EmailClient, SubscriberEmail};
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::{faker::internet::en::SafeEmail, Fake};
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() -> Result<(), Box<dyn std::error::Error>> {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake())?;
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake())?;
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        email_client
+            .send_email(&subscriber_email, &subject, &content, &content)
+            .await?;
+
+        Ok(())
     }
 }
