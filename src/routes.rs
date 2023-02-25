967385c848616b7b3e45c1e8615d3292af3788f9
galah92 <galaharoni92@gmail.com>
2023-02-25 13:52:59 +0000

    settings status of new subscriptions to 'confirmed'

--- src/routes.rs
+++ src/routes.rs
@@ -39,8 +39,8 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
     };
     sqlx::query!(
         r#"
-            INSERT INTO subscriptions (id, email, name, subscribed_at)
-            VALUES ($1, $2, $3, $4)
+            INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+            VALUES ($1, $2, $3, $4, 'confirmed')
             "#,
         Uuid::new_v4(),
         subscriber.email.as_ref(),
